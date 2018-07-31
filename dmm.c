154148eb7dd2b741e780c9a4a834c82b5965320d
TharaVeera <thara.veeramachaneni@gmail.com>
2018-07-30 20:46:46 -0700

    Cleaned up comments in dmm.c

--- dmm.c
+++ dmm.c
@@ -1,29 +1,18 @@
-#include <stdio.h>  // needed for size_t
-#include <unistd.h> // needed for sbrk
-#include <assert.h> // needed for asserts
+#include <stdio.h>  
+#include <unistd.h> 
+#include <assert.h> 
 #include "dmm.h"
 
-/* You can improve the below metadata structure using the concepts from Bryant
- * and OHallaron book (chapter 9).
- */
-
+/* Metadata stores info about heap blocks not available to the user.  It is for the heap manager to maintain which blocks 
+are free and how many free bytes they have */
 typedef struct metadata {
-  /* size_t is the return type of the sizeof operator. Since the size of an
-   * object depends on the architecture and its implementation, size_t is used
-   * to represent the maximum size of any object in the particular
-   * implementation. size contains the size of the data object or the number of
-   * free bytes
-   */
   size_t size;
   bool freed; 
   struct metadata* next;
   struct metadata* prev; 
 } metadata_t;
 
-/* freelist maintains all the blocks which are not in use; freelist is kept
- * sorted to improve coalescing efficiency 
- */
-
+/*The free list preserves which blocks are in use/not in use*/
 static metadata_t* freelist = NULL;
 metadata_t * head = NULL; 
 metadata_t * endlist = NULL;
@@ -31,27 +20,24 @@ int s = 440;
 char *end;
 
 metadata_t* split (metadata_t * curr, int numbytes){
-  //0x1c84e90 -> 0x1c85070
   size_t size = sizeof(metadata_t) + numbytes;
+  /*If the current block's size minus the size needed is less than the size of metadata, return the pointer. 
+    This prevents a split that would result in a block that is smaller than a metadata, which could never be
+    allocated. */
   if((curr->size - size) <= sizeof(metadata_t)){
     return curr; 
   }
-  metadata_t* header1 = curr; //making a header1 to start of block to be returned
+  metadata_t* header1 = curr; //Initializing header for new block to be returned
   size_t oldsize = curr->size; 
   metadata_t* next = curr->next; 
-  curr = curr +1; //moved past the header; 
+  curr = curr +1; //Move pointer past the header 
   char *ptr = (char*) curr; 
-
   ptr = ptr + numbytes; 
-  if(s == numbytes){
-    //printf("ptr: %p\n", ptr);
-  }
 
-  curr = (metadata_t*)ptr; //restore struct pointer from ptr
-  metadata_t* header2 = curr; //start header for leftover block
-  //initialize new free block 
+  curr = (metadata_t*)ptr; //Restore pointer to metadata type
+  /*Initialize new free block and block to be returned*/
+  metadata_t* header2 = curr; //Create header for leftover block
   header2->size = oldsize - (numbytes + sizeof(metadata_t)); 
-  //header2->size = next - header2;
   header1->size = oldsize - header2->size; 
   header1->next = header2; 
   header2->prev = header1; 
@@ -66,56 +52,40 @@ metadata_t* split (metadata_t * curr, int numbytes){
 
 }
 
-void* dmalloc(size_t numbytes) {
-  /* initialize through sbrk call first time */
-  //printf("numbytes: %zu\n", numbytes);
-  //fflush(stdout);
+/* Allocate memory region for desired number of bytes */
+void* dmalloc(size_t numbytes) { 
   
   if(freelist == NULL) { 			
-    if(!dmalloc_init())
+    if(!dmalloc_init()) //Initializes heap region through sbrk call
       return NULL;
   }
-/* your code here */
-  assert(numbytes > 0);
-  numbytes = ALIGN(numbytes);
-  //printf("numbytes: %zu\n", numbytes);
-  metadata_t* curr = freelist; 
-  //printf("Curr: %p", curr);
-  //fflush(stdout);
-  while(curr !=NULL){
 
+  assert(numbytes > 0);
+  numbytes = ALIGN(numbytes); //Align desired number of bytes according to wordsize = 8 bytes
+  metadata_t* curr = freelist; 
+
+  while(curr !=NULL){
+    /*If the size of the block is equal to what is needed, set it to free and return the pointer */ 
     if(curr->size == (numbytes + sizeof(metadata_t)) && curr->freed){
-      //printf("is equal\n");
       curr->freed = false;
-      return (void*)(curr +1); //points to byte right after header
+      return (void*)(curr +1); //Point to byte after metdata
     }
+    /*If the size of the block is greater than what is needed, split it, set that block to free, and return the pointer */
     if((sizeof(metadata_t) + numbytes) < curr->size  && curr->freed){
-      //printf("size: %zu\n", sizeof(metadata_t) + numbytes);
-      //printf("Comparing less than size: \n");
       curr = split(curr, numbytes); 
       curr->freed = false;
-      return (void*)(curr +1);//points to byte right after header
+      return (void*)(curr +1); //Points to byte after metadata
     }
   curr = curr->next; 
   }
   return NULL;
   }
-  /*
- metadata_t* tail = curr; 
- metadata_t* p = sbrk(sizeof(metadata_t) + numbytes); //size of header + size of payload 
- p->size = sizeof(metadata_t) + numbytes; 
- p->freed = false; 
- p->prev = tail; 
- p->next = NULL; 
- //ALIGN? 
- //don't have to coalesce i guess
- return (void *)(p +1);
- */
+
+
 void coalesce(metadata_t * curr){
   int totalsize; 
-  //if at the front of the list
-  //printf("freelist: %p\n", freelist);
-  //printf("curr: %p\n", curr);
+
+/*If at the front of the list, check if the next block is free, then coalesce */
   if(curr == head){
     if(curr->next->freed){
       totalsize = curr->size + curr->next->size; 
@@ -127,6 +97,7 @@ void coalesce(metadata_t * curr){
     }
     return; 
   }
+  /*If at the end of the list, check if the previous block is free, then coalesce */
   if(curr == endlist){
     if(curr->prev->freed){
       totalsize = curr->size + curr->prev->size; 
@@ -136,7 +107,7 @@ void coalesce(metadata_t * curr){
     }
     return;
   }
-
+/*If in the middle of the list, check if the previous and next block are free, then coalesce*/
   if(curr->prev != NULL){
       if(curr->prev->freed){
         totalsize = curr->size + curr->prev->size; 
@@ -164,26 +135,19 @@ void coalesce(metadata_t * curr){
 
 
 void dfree(void* ptr) {
-  /* your code here */
   if(ptr == NULL){
     return;
   }
   head = freelist; 
-  //printf("ptr: %p\n: ", ptr);
-  ptr = ptr - sizeof(metadata_t);
-  //printf("ptr: %p\n", ptr);
+  /*Move back the number of bytes for a metadata to point to beginning of a metadata block and iterate over freelist */
+  ptr = ptr - sizeof(metadata_t); 
   metadata_t * curr = freelist; 
-  //printf("freelist: %p\n", freelist);
+
   while(curr != NULL){
-    if(ptr == curr && !curr->freed){
-      //printf("ptr = curr: %p\n: ", ptr);
-      //printf("curr: %p\n", curr);
+      /*If the current block pointer equals the desired pointer and it is in use, set it free*/
+    if(ptr == curr && !curr->freed){ 
       curr->freed = true;
-      //printf("ptr->freed: %d\n: ", curr->freed); 
-      //printf("about to be free\n");
-      //printf("freelist: %p\n", freelist);
-      //printf("curr: %p\n", curr);
-      coalesce(curr);
+      coalesce(curr); //Coalesce to create bigger free blocks for later dmalloc use 
       return;
     }
     curr = curr->next; 
@@ -192,40 +156,22 @@ void dfree(void* ptr) {
 }
 
 bool dmalloc_init() {
-
-  /* Two choices: 
-   * 1. Append prologue and epilogue blocks to the start and the
-   * end of the freelist 
-   *
-   * 2. Initialize freelist pointers to NULL
-   *
-   * Note: We provide the code for 2. Using 1 will help you to tackle the 
-   * corner cases succinctly.
-   */
-
-  //printf("Max heap size: %d\n", MAX_HEAP_SIZE);
-  size_t max_bytes = ALIGN(MAX_HEAP_SIZE);
-  //printf("max bytes: %zu\n", max_bytes);
-  /* returns heap_region, which is initialized to freelist */
-  freelist = (metadata_t*) sbrk(max_bytes); //16449536
+  size_t max_bytes = ALIGN(MAX_HEAP_SIZE); //Set the max heap size, and align it to the word size
+  freelist = (metadata_t*) sbrk(max_bytes); //Return the heap region and intializes the freelist
   end = (char*) freelist; 
-  end = end + max_bytes; //16450560, 16450560-16449536 = 1024
-  endlist = (metadata_t*)end;
-  //printf("end ptr:%p\n", end);
-  //printf("freelist ptr: %p\n",freelist);
-  //fflush(stdout);
-  /* Q: Why casting is used? i.e., why (void*)-1? */
+  end = end + max_bytes; 
+  endlist = (metadata_t*)end; //Store pointer to the end of the list
+
   if (freelist == (void *)-1)
     return false;
   freelist->next = NULL;
   freelist->prev = NULL;
   freelist->freed = true; 
   freelist->size = max_bytes-METADATA_T_ALIGNED;
-  //printf("freelist size: %zu\n",freelist->size  );
   return true;
 }
 
-/* for debugging; can be turned off through -NDEBUG flag*/
+//debugging purposes
 void print_freelist() {
   metadata_t *freelist_head = freelist;
   while(freelist_head != NULL) {
